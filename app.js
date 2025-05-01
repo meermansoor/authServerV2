f19d08e14f791c9840e277991a6a96ceecc4b911
meermansoor <meermansoor002@gmail.com>
2025-05-02 01:47:36 +0500

    added bcrypt and JWT (intial)

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ import userRouter from './routes/user.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import authRouter from './routes/auth.routes.js';
 import connectDB from './database/mongoDB.js';
-
+import errorMiddleWare from './middleware/error.middleware.js';
+import cookieParser from 'cookie-parser';
 
 
 
@@ -14,6 +15,11 @@ app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/subscription', subscriptionRouter);
 app.use('/api/v1/subscription/upcomingrenewals', subscriptionRouter);
+
+app.use(errorMiddleWare); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 
 app.get('/', (req, res) => {
